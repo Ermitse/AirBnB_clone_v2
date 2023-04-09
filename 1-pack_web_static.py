984edbb84fb6fde52deb85c52b1f508e2f26652e
ermitse <ermiastse@gmail.com>
2023-04-09 03:11:17 -0700

    fabscript

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Fabfile to create a .tgz archive """
+""" Fabscript to create a .tgz archive """
 import tarfile
 from datetime import datetime
 import os
 
 
 def do_pack():
-	""" Creates a .tgz archive """
+	"""to create the .tgz archive """
 	date = datetime.now().strftime("%Y%m%d%H%M%S")
 	filename = "versions/web_static_{}.tgz".format(date)
 	if not os.path.exists("versions/"):
