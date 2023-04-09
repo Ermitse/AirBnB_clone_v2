68932090b4daba0eabdd10b5422a3b2ff13278e2
ermitse <ermiastse@gmail.com>
2023-04-09 03:08:22 -0700

    set up web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Sets up web servers for the deployment of web stat
-# - Installs Nginx
-# - Creates /data/, /data/web_static, /data/web_static/releases/,
-# /data/web_static/releases/test, /data/web_static/releases/test/index.html
-# - Creates a sym link /data/web_static/current -> /data/web_static/releases/test/
-# - grants /data/ -> ubuntu and group
-# - updates nginx conf to serve /data/web_static/current/
-# - restarts nginx
+# - this will Install Nginx
+# - Create a /data/, /data/web_static, /data/web_static/releases/,
+#   /data/web_static/releases/test, /data/web_static/releases/test/index.html
+# - Create a sym link /data/web_static/current -> /data/web_static/releases/test/
+# - grants /data/ ->(to) ubuntu and group
+# - update nginx conf to serve /data/web_static/current/
+# - finally restart nginx
 apt-get -y update
 apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
